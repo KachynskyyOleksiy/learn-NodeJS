27a44621b5fb5c915cd09081309c745f077ce197
Oleksiy <k.oleksiy.v@gmail.com>
2016-03-26 22:11:36 +0200

    Start simple server

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log("Hello world!");
+var http = require('http');
+http.createServer(function(request, response) {
+  response.writeHead(200);
+  response.write("Hello, this is dog.");
+  response.end();
+}).listen(8080);
+console.log("Listening on port 8080...");
