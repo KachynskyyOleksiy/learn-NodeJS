355d329b7cab33ac245eca448896c0d884793ea5
Oleksiy <k.oleksiy.v@gmail.com>
2016-03-27 01:07:05 +0200

    remove timeiout

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ var port = process.env.PORT || 8080;
 http.createServer(function(request, response) {
   response.writeHead(200);
   response.write("Dog is running...\n");
-  setTimeout(function() {
-    response.write("Dog finish!\n");
-    response.end();
-  }, 4000);
+  response.write("Dog finish!\n");
+  response.end();
 }).listen(port);
 
 console.log("Listening on port 8080...");
