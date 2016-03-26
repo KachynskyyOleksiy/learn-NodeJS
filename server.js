02e58f0d1d7929d01330cfcf93ea26c549ea73bb
Oleksiy <k.oleksiy.v@gmail.com>
2016-03-27 00:22:43 +0200

    try timeout

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var http = require('http');
 
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 8080;
+
 http.createServer(function(request, response) {
   response.writeHead(200);
-  response.write("Hello, this is dog.");
-  response.end();
+  response.write("Dog is running...\n");
+  setTimeout(function() {
+    response.write("Dog finish!\n");
+    response.end();
+  }, 4000);
 }).listen(port);
+
 console.log("Listening on port 8080...");
