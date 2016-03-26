00d2c57aceca68081e754634aea0c417f5e7992f
Oleksiy <k.oleksiy.v@gmail.com>
2016-03-26 23:18:41 +0200

    add env.port

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var http = require('http');
+
+var port = process.env.PORT || 8080
 http.createServer(function(request, response) {
   response.writeHead(200);
   response.write("Hello, this is dog.");
   response.end();
-}).listen(8080);
+}).listen(port);
 console.log("Listening on port 8080...");
