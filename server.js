adb004a9737bb763182615da780cc0d7f231233b
Oleksiy <k.oleksiy.v@gmail.com>
2016-03-27 23:57:22 +0300

    try socket.io locally

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
+// Load modules
 var express = require('express');
 var app = express();
 
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+// Settings
 // process.env.PORT is for using env settings, for example deploy on heroku
 var port = process.env.PORT || 8080;
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/public'));
 
+// Routes
 app.get('/', function(request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(port);
+// Socket
+io.on('connection', function(client){
+  console.log('Client connected...');
+});
 
-console.log("Express listen on port "+ port +"...");
+// Start server
+server.listen(port, function() {
+  console.log("Server listen on port "+ port +"...");
+});
