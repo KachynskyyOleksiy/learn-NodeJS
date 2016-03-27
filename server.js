994e96826563730f958bd3a50a7928a1c6088bd5
Oleksiy <k.oleksiy.v@gmail.com>
2016-03-27 12:54:25 +0300

    read from request stream and write to response stream

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ var port = process.env.PORT || 8080;
 
 http.createServer(function(request, response) {
   response.writeHead(200);
-  response.write("Dog is running...\n");
-  response.write("Dog finish!\n");
-  response.end();
+  request.on('readable', function() {
+    var chunk = null;
+    while (null !== (chunk = request.read())) {
+      response.write(chunk);
+      console.log(chunk.toString());
+    }
+  });
+  request.on('end', function() {
+    response.end();
+    console.log("response.end");
+  });
 }).listen(port);
 
 console.log("Listening on port "+ port +"...");
+
+//type in command line
+//curl -d "hello" localhost:8080
