927f0beedff9784de21de1bf7c7471ba3a6b279b
Oleksiy <k.oleksiy.v@gmail.com>
2016-03-27 13:13:11 +0300

    read from ReadStream end write to WriteStream

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ var port = process.env.PORT || 8080;
 
 http.createServer(function(request, response) {
   response.writeHead(200);
-  request.pipe(response);
-}).listen(port);
+  response.write('Hello from L3');
+  response.end();
+});
 
-console.log("Listening on port "+ port +"...");
+//console.log("Listening on port "+ port +"...");
 
-//type in command line
-//curl -d "hello" localhost:8080
+var fs = require('fs'); //require filestream module
+
+var file = fs.createReadStream('README.md');
+var newFile = fs.createWriteStream('README_copy.md');
+
+file.pipe(newFile);
+console.log("Content of " + file.path + "was copied to " + newFile.path);
