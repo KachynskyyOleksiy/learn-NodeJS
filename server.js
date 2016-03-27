66c1cbd16f72b893699fc411ad344949f7f17b1e
Oleksiy <k.oleksiy.v@gmail.com>
2016-03-27 12:59:24 +0300

    use pipe for send request to client back

--- server.js
+++ server.js
@@ -3,17 +3,7 @@ var port = process.env.PORT || 8080;
 
 http.createServer(function(request, response) {
   response.writeHead(200);
-  request.on('readable', function() {
-    var chunk = null;
-    while (null !== (chunk = request.read())) {
-      response.write(chunk);
-      console.log(chunk.toString());
-    }
-  });
-  request.on('end', function() {
-    response.end();
-    console.log("response.end");
-  });
+  request.pipe(response);
 }).listen(port);
 
 console.log("Listening on port "+ port +"...");
