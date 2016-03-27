509b46851bc3206abe6cff179a5c71aea63390b0
Oleksiy <k.oleksiy.v@gmail.com>
2016-03-28 01:43:08 +0300

    Send message from client to server. Add bootstrap and jquery to bower

--- public/js/client.js
+++ public/js/client.js
@@ -2,3 +2,9 @@ var socket = io.connect('http://localhost:8080');
 socket.on('messages', function(data) {
   console.log(data.hello);
 });
+
+$('#chat_form').submit(function (e) {
+  var message = $('#input_text').val();
+  socket.emit('messages', message);
+  return false;
+});
