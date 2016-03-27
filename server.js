83672f9f0e5f73e0db0ba5f359be8be5c5af159c
Oleksiy <k.oleksiy.v@gmail.com>
2016-03-27 11:59:18 +0300

    Learning events

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 var http = require('http');
-
 var port = process.env.PORT || 8080;
 
-http.createServer(function(request, response) {
+//alternate syntax
+var server = http.createServer();
+server.on('request', function(request, response) {
   response.writeHead(200);
   response.write("Dog is running...\n");
   response.write("Dog finish!\n");
   response.end();
-}).listen(port);
+});
 
-console.log("Listening on port 8080...");
+server.on('close', function(){console.log('Close')});
+
+server.listen(port);
+console.log("Listening on port " + port + "...");
+
+server.close();
+
+//custom event emmiters
+var EventEmitter = require('events').EventEmitter;
+var logger = new EventEmitter();
+logger.on('error', function(message){
+  console.log('ERR' + message);
+});
+logger.emit('error', "Spilled Milk");
