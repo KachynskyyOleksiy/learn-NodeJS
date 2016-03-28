212c31aa3753826c37dfe4e808d8e61a91ae48f6
Oleksiy <k.oleksiy.v@gmail.com>
2016-03-28 10:29:21 +0300

    save message history in RAM

--- server.js
+++ server.js
@@ -19,25 +19,39 @@ app.get('/chatroom', function(request, response) {
   response.sendFile(__dirname + "/chatroom.html");
 });
 
+
+var messages = []; //store messages in array
+
+var storeMessage = function(name, data){
+  messages.push( {name: name, data: data} ); // add message to end of array
+  if (messages.length > 8) {
+    messages.shift(); //if more than 8 messages, remove the firs one
+  }
+};
+
 // Socket
 io.on('connection', function(client){
-
 
   //set the nickname associated with this client
   client.on('join', function(name) {
     client.nickname = name;
     console.log(client.nickname + ' join to chat');
+    messages.forEach(function(message) {
+      client.emit("messages", message.name + ": " + message.data);
+      // iterate through messages array and emit a message on the connecting client for each one
+    });
   });
 
   //emit the messages event on the client
   client.emit('messages', "Hello in our chatroom");
   
   //listen for 'messages' events from clients
-  client.on('messages', function(data) {
-    console.log(data);
+  client.on('messages', function(message) {
+    console.log(message);
     //broadcats mesage to all other clients connected
-    client.broadcast.emit('messages', client.nickname + ": "+ data);
-    client.emit('messages', client.nickname + ": "+ data);
+    client.broadcast.emit('messages', client.nickname + ": "+ message);
+    client.emit('messages', client.nickname + ": "+ message);
+    storeMessage(client.nickname, message);
   });
 
 });
